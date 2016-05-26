37f976443f174342e878f178b34f36d38ecc797f
Paul DeCarlo <pdecarlo@microsoft.com>
2016-05-26 16:20:38 -0500

    Add BotConnector sample

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'process.env.APP_ID', appSecret: 'process.env.APP_SECRET' });
-bot.add('/', function (session) {
-    session.send('Hello World');
-});
-
-// Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+
+var helloBot = new builder.BotConnectorBot();
+helloBot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault(function (session) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            session.send('Hello %s!', session.userData.name);
+        }
+    }));
+helloBot.add('/profile',  [
+    function (session) {
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your name?');
+        }
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+server.use(helloBot.verifyBotFramework({ appId: 'process.env.APP_ID', appSecret: 'process.env.APP_SECRET'  }));
+server.post('/api/messages', helloBot.listen());
+
+server.listen(8080, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
